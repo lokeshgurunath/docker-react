53281ec73297e7d19f977c2551c0bbd19967744b
Lokesh <lokesh.gurunath@gmail.com>
2020-07-16 13:29:18 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <br/>Modified by <b>Lokesh</b> on July 14th
         </p>
         <a
           className="App-link"
